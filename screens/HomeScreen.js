9509da626ec99471e03ba41691440d56a70f3b46
Khulon <e0725788@u.nus.edu>
2022-11-29 22:32:06 +0800

    cleon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,7 +8,7 @@ const HomeScreen = () => {
         <View>
             <Text className="text-red-400">HomeScreen</Text>
             <Button
-                title="Go to Profileeee"
+                title="Go to Profile"
                 onPress={()=>navigation.navigate("Profile")}
             />
         </View>
