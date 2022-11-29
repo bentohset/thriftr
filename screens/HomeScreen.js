1936c9061baa1182d711fed319bd5b732d005f72
Benjamin Toh <toh.benjamin123@gmail.com>
2022-11-29 21:18:25 +0800

    +

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import { View, Text ,Button} from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-400">HomeScreen</Text>
+      <Button
+        title="Gg"
+        color="#841584"
+        />
+    </View>
+  )
+}
+
+export default HomeScreen
