6410c3ba1c49f6eef2f78d6dca71531e04df0660
Benjamin Toh <toh.benjamin123@gmail.com>
2022-12-03 23:57:33 +0800

    email auth update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,48 @@
-import { View, Text ,Button} from 'react-native'
+import { View, Text ,Button, SafeAreaView} from 'react-native'
 import { useNavigation } from '@react-navigation/native';
-import React from 'react'
+import React, { useEffect , useState } from 'react'
+import { firebase } from '../firebase';
+import useAuth from "../hooks/useAuth";
 
 const HomeScreen = () => {
     const navigation = useNavigation();
+    const {logout} = useAuth();
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        firebase.firestore().collection('users')
+        .doc(firebase.auth().currentUser.uid).get()
+        .then((snapshot) => {
+            if(snapshot.exists){
+                setName(snapshot.data())
+            }
+            else {
+                console.log('User does not exist')
+            }
+        })
+    }, [])
+
     return (
-        <View>
-            <Text className="text-red-400">HomeScreen</Text>
+        <SafeAreaView>
+            <Text className="text-red-400">
+                Hello, {name.firstName}
+            </Text>
+            <TouchableOpacity onPress={() => {firebase.auth().signOut()}}>
+                <Text>
+                    Sign out
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                <Text>
+                    settings
+                </Text>
+            </TouchableOpacity>
             <Button
                 title="Go toooo Profile helloworld"
                 onPress={()=>navigation.navigate("Profile")}
             />
-        </View>
+            <Button title="Logout" onPress={logout}/>
+        </SafeAreaView>
     )
 }
 
